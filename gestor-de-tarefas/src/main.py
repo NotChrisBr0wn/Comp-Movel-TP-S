978632edc11a79e113d6242b8f6cc8aa7694b61e
Dani3l <batistadaniel3007@gmail.com>
2026-02-12 16:28:35 +0000

    feat: created basic todo page controls

--- gestor-de-tarefas/src/main.py
+++ gestor-de-tarefas/src/main.py
@@ -1,6 +1,16 @@
-import flet as ft
+import flet
+from flet import Checkbox, FloatingActionButton, Icons, Page, TextField
 
-def main(page: ft.Page):
-    page.add(ft.Text(value="Hello, World!"))
 
-ft.run(main)
+def main(page: Page):
+    def add_clicked(e):
+        page.add(Checkbox(label=new_task.value))
+        new_task.value = ""
+        page.update()
+
+    new_task = TextField(hint_text="Whats needs to be done?")
+
+    page.add(new_task, FloatingActionButton(icon=Icons.ADD, on_click=add_clicked))
+
+
+flet.run(main)

45e8e5cc0adf95b970c59efdbdf73de08f4641f0
Dani3l <batistadaniel3007@gmail.com>
2026-02-12 16:36:07 +0000

    feat: added reusable ui components and an option to view, edit and remove taks

--- gestor-de-tarefas/src/main.py
+++ gestor-de-tarefas/src/main.py
@@ -1,29 +1,115 @@
+from dataclasses import field
+from typing import Callable
+
 import flet as ft
 
 
-def main(page: ft.Page):
-    def add_clicked(e):
-        tasks_view.controls.append(ft.Checkbox(label=new_task.value))
-        new_task.value = ""
-        view.update()
+@ft.control
+class Task(ft.Column):
+    task_name: str = ""
+    on_task_delete: Callable[["Task"], None] = field(default=lambda task: None)
+    
+    # Task Control containing a checkbox to display the task and two icon buttons to edit and delete the task
+    def init(self):
+        self.display_task = ft.Checkbox(value=False, label=self.task_name)
+        self.edit_name = ft.TextField(expand=1)
+        
+        # Container with the task name and the edit and delete buttons, aligned to the right of the task name
+        self.display_view = ft.Row(
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+            controls=[
+                self.display_task,
+                ft.Row(
+                    spacing=0,
+                    controls=[
+                        ft.IconButton(
+                            icon=ft.Icons.CREATE_OUTLINED,
+                            tooltip="Edit To-Do",
+                            on_click=self.edit_clicked,
+                        ),
+                        ft.IconButton(
+                            ft.Icons.DELETE_OUTLINE,
+                            tooltip="Delete To-Do",
+                            on_click=self.delete_clicked,
+                        ),
+                    ],
+                ),
+            ],
+        )
+         # Task Editor
+        self.edit_view = ft.Row(
+            visible=False,
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
+            controls=[
+                self.edit_name,
+                ft.IconButton(
+                    icon=ft.Icons.DONE_OUTLINE_OUTLINED,
+                    icon_color=ft.Colors.GREEN,
+                    tooltip="Update To-Do",
+                    on_click=self.save_clicked,
+                ),
+            ],
+        )
+        self.controls = [self.display_view, self.edit_view]
 
-    new_task = ft.TextField(hint_text="What needs to be done?", expand=True)
-    tasks_view = ft.Column()
-    view = ft.Column(
-        width=600,
-        controls=[
+    def edit_clicked(self, e):
+        self.edit_name.value = self.display_task.label
+        self.display_view.visible = False
+        self.edit_view.visible = True
+        self.update()
+
+    def save_clicked(self, e):
+        self.display_task.label = self.edit_name.value
+        self.display_view.visible = True
+        self.edit_view.visible = False
+        self.update()
+
+    def delete_clicked(self, e):
+        self.on_task_delete(self)
+
+
+@ft.control
+class TodoApp(ft.Column):
+    # application's root control is a Column containing all other controls
+    def init(self):
+        self.new_task = ft.TextField(hint_text="What needs to be done?", expand=True)
+        self.tasks = ft.Column()
+        self.width = 600
+        self.controls = [
             ft.Row(
                 controls=[
-                    new_task,
-                    ft.FloatingActionButton(icon=ft.Icons.ADD, on_click=add_clicked),
+                    self.new_task,
+                    ft.FloatingActionButton(
+                        icon=ft.Icons.ADD, on_click=self.add_clicked
+                    ),
                 ],
             ),
-            tasks_view,
-        ],
-    )
+            self.tasks,
+        ]
 
+    def add_clicked(self, e):
+        task = Task(task_name=self.new_task.value, on_task_delete=self.task_delete)
+        self.tasks.controls.append(task)
+        self.new_task.value = ""
+        self.update()
+
+    def task_delete(self, task):
+        self.tasks.controls.remove(task)
+        self.update()
+
+
+def main(page: ft.Page):
+    page.title = "To-Do App"
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-    page.add(view)
+    page.update()
+
+    # create application instance
+    app = TodoApp()
+
+    # add application's root control to the page
+    page.add(app)
 
 
 ft.run(main)

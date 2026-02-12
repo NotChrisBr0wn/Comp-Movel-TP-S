1280147a7cae6251f789007677e0dfee7f7b6930
Dani3l <batistadaniel3007@gmail.com>
2026-02-12 17:04:06 +0000

    fix: fixed error - 'TabBar' object has no attribute 'selected_index'

--- gestor-de-tarefas/src/main.py
+++ gestor-de-tarefas/src/main.py
@@ -7,15 +7,16 @@ import flet as ft
 @ft.control
 class Task(ft.Column):
     task_name: str = ""
-    on_task_status_change: Callable[["Task"], None] = field(default=lambda task: None)
-    on_task_delete: Callable[["Task"], None] = field(default=lambda task: None)
-    
-    # Task Control containing a checkbox to display the task and two icon buttons to edit and delete the task
+    on_status_change: Callable[[], None] = field(default=lambda: None)
+    on_delete: Callable[["Task"], None] = field(default=lambda task: None)
+
     def init(self):
-        self.display_task = ft.Checkbox(value=False, label=self.task_name)
+        self.completed = False
+        self.display_task = ft.Checkbox(
+            value=False, label=self.task_name, on_change=self.status_changed
+        )
         self.edit_name = ft.TextField(expand=1)
-        
-        # Container with the task name and the edit and delete buttons, aligned to the right of the task name
+
         self.display_view = ft.Row(
             alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
             vertical_alignment=ft.CrossAxisAlignment.CENTER,
@@ -38,7 +39,7 @@ class Task(ft.Column):
                 ),
             ],
         )
-         # Task Editor
+
         self.edit_view = ft.Row(
             visible=False,
             alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
@@ -67,21 +68,21 @@ class Task(ft.Column):
         self.edit_view.visible = False
         self.update()
 
-    def delete_clicked(self, e):
-        self.on_task_delete(self)
-
     def status_changed(self, e):
         self.completed = self.display_task.value
-        self.task_status_change()
+        self.on_status_change()
+
+    def delete_clicked(self, e):
+        self.on_delete(self)
 
 
 @ft.control
 class TodoApp(ft.Column):
     # application's root control is a Column containing all other controls
     def init(self):
-        self.new_task = ft.TextField(hint_text="What needs to be done?", expand=True)
+        self.new_task = ft.TextField(hint_text="Whats needs to be done?", expand=True)
         self.tasks = ft.Column()
-        
+
         self.filter = ft.TabBar(
             scrollable=False,
             tabs=[
@@ -108,7 +109,13 @@ class TodoApp(ft.Column):
                     ),
                 ],
             ),
-            self.tasks,
+            ft.Column(
+                spacing=25,
+                controls=[
+                    self.filter_tabs,
+                    self.tasks,
+                ],
+            ),
         ]
 
     def add_clicked(self, e):
@@ -121,24 +128,24 @@ class TodoApp(ft.Column):
         self.new_task.value = ""
         self.update()
 
+    def task_status_change(self):
+        self.update()
+
     def task_delete(self, task):
         self.tasks.controls.remove(task)
         self.update()
 
-    def tabs_changed(self, e):
-        self.update()
-
-    def task_status_change(self, e):
-        self.update()
-
     def before_update(self):
-        status = self.filter.tabs[self.filter.selected_index].text
+        status = self.filter.tabs[self.filter_tabs.selected_index].label
         for task in self.tasks.controls:
             task.visible = (
                 status == "all"
                 or (status == "active" and not task.completed)
                 or (status == "completed" and task.completed)
             )
+
+    def tabs_changed(self, e):
+        self.update()
 
 
 def main(page: ft.Page):
